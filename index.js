acc0859db46283fc78cd35d04529866acb0713a5
CSchepi <CSchepi@users.noreply.github.com>
2021-05-17 23:04:09 +0200

    Website

---
+++ index.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', () => {
+    let scroll = window.pageYOffset;
+    var factor1 = "0px "+(100-((scroll+900)/30))+"%";
+    var factor2 = "0px "+(100-((scroll+200)/30))+"%";
+    var factor3 = "0px "+(100-((scroll-500)/30))+"%";
+    var factor4 = "0px "+(100-((scroll-1200)/30))+"%";
+    var factor5 = "0px "+(100-((scroll-1900)/30))+"%";
+    document.getElementById("zb1").style.backgroundPosition =factor1;
+    document.getElementById("zb2").style.backgroundPosition =factor2;
+    document.getElementById("zb3").style.backgroundPosition =factor3;
+    document.getElementById("zb4").style.backgroundPosition =factor4;
+    document.getElementById("zb5").style.backgroundPosition =factor5;
+    console.log(factor2);
+});
